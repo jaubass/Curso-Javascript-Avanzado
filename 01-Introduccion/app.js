e21152056204b02dcd2d25e81175df53d68db06d
jaubass <jaubass@gmail.com>
2023-10-28 21:14:25 +0200

    Agregado código inicial del proyecto

---
+++ 01-Introduccion/app.js
@@ -0,0 +1,10 @@
+/*
+Comentarios de mas de una linea
+*/
+const nombre = prompt('Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo Javascript Moderno`; 
+
+// Asigna el valor a la variable producto 
+const producto = 'Tv'
+
+console.log(Hi)
---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,16 @@
+/*
+VARIABLES -> Javascript es un lenguage dinámico no exite el tipado.
+*/
+
+// Inicializar veriables con valor
+var producto = "Tele de 24 pulados"
+console.log(producto)
+
+// Las variables se pueden reasignar
+producto = 2;
+console.log(producto)
+
+
+
+
+
